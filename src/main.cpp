03eaec08ac3e3cc481ab1db69bb05c648b229bc2
Ingomar Otter <io@kgbvax-mbp20.local>
2020-10-04 15:43:53 +0200

    foo

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,26 @@
 #include <M5Stack.h>
 #include "time.h"
 
+//I/O
 #include "SparkFun_SCD30_Arduino_Library.h"
-#include <Tomoto_HM330X.h>
+#include <Tomoto_HM330X.h> 
+#include <FastLED.h>
 
+//networking
 #include <WiFi.h>
-#include <WiFiClientSecure.h>
+#include <WiFiClient.h>
+#include <HTTPClient.h>
 
+//not in SCM, provide your own defining WIFI_SSID, WIFI_PASSWORD
 #include "envs.h"
 
-#include <FastLED.h>
+//we dweet right now
+//#include "dweet.h"
 #include <ArduinoJson.h>
-#include "dweet.h"
 
+//for watchdog
 #include "esp_system.h"
 const int loopTimeCtl = 0;
-
 hw_timer_t *timer = NULL;
 
 void IRAM_ATTR resetModule()
@@ -31,11 +36,10 @@ void IRAM_ATTR resetModule()
 Tomoto_HM330X *pMSensor;
 // Tomoto_HM330X sensor(Wire1); // to use the alternative wire
 
+//M5Stack onboard leds
 #define M5STACK_FIRE_NEO_NUM_LEDS 10
 #define M5STACK_FIRE_NEO_DATA_PIN 15
-
 CRGB leds[M5STACK_FIRE_NEO_NUM_LEDS];
-
 TaskHandle_t Task1;
 
 #define USE_WIFI
@@ -44,9 +48,9 @@ const char *password{WIFI_PASSWORD}; // write your WiFi password
 const char *ntpServer = "europe.pool.ntp.org";
 const long gmtOffset_sec = 3600;
 const int daylightOffset_sec = 3600;
-const char *dweetThingName = "ottermuehle-co2ampel";
+const String dweetThingName = "ottermuehle-co2ampel-2";
 
-const int watchdogTimeoutUs = 9000000; //in usec
+const int watchdogTimeoutUs = 15000000; //in usec
 const int loopDelay = 5000;            //main loop() delay
 
 // sensor setting
@@ -60,7 +64,7 @@ SCD30 airSensor;
 #define SPRITE_WIDTH 320
 #define SPRITE_HEIGHT 160
 
-// CO2 level setting
+// CO2 limits/setting
 #define CO2_MIN_PPM 0
 #define CO2_MAX_PPM 2500
 #define CO2_RANGE_INT 500
@@ -303,21 +307,41 @@ void updateDisplay()
 
 void doDweet()
 {
-    StaticJsonDocument<400> doc;
+    StaticJsonDocument<512> doc;
+    
     doc["co2_ppm"] = airSensor.getCO2();
+    doc["humidity_pct"] = airSensor.getHumidity();
+    doc["temp_DegC"] = airSensor.getTemperature();
+
+    doc["PM1"] = pMSensor->std.getPM1();
+    doc["PM2_5"] = pMSensor->std.getPM2_5();
+    doc["PM10"] = pMSensor->std.getPM10();
+    
+
+    struct tm timeinfo;
+    if (getLocalTime(&timeinfo))
+    {
+        doc["local-timestamp"] =  String(timeinfo.tm_hour) + String(":")+ String(timeinfo.tm_min) + String(":")+ String(timeinfo.tm_sec);
+    }
 
     String output;
     serializeJson(doc, output);
 
-    WiFiClientSecure secureClient;
+    WiFiClient client;
 
-    Dweet *d = new Dweet(secureClient);
-    d->dweet(dweetThingName, output.c_str());
+    HTTPClient http;
+    String base="http://dweet.io:80/dweet/for/";
+    
+    http.begin(base+dweetThingName);
+    http.addHeader("X-DWEED-AUTH",X_DWEED_AUTH);
+    http.setUserAgent("net.kgbvax.co2ampelard");
+    http.addHeader("accept","application/json");
+    http.addHeader("content-type","application/json");
+    http.POST(output.c_str());
 }
 
 void loop()
 {
-
     if (airSensor.dataAvailable())
     {
         // get sensor data
